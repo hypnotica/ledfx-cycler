b69a843f4d1ee9d4b77626c508328ddecf8be142
Logan I <8002234+hypnotica@users.noreply.github.com>
2021-03-25 01:35:55 -0700

    Removed casting of seconds_per_scene to int
    Allows for sub-second values

--- ledfx-cycler.py
+++ ledfx-cycler.py
@@ -4,14 +4,14 @@ import time
 import sys
 import requests
 
-time_per_scene = 30
+seconds_per_scene = 30
 scenes_url = "http://127.0.0.1:8888/api/scenes"
 scenes = requests.get(scenes_url)
 scenes.raise_for_status()
 scenes_dict = scenes.json()
 
 if len(sys.argv) >= 2 and sys.argv[1].isdigit():
-    time_per_scene = sys.argv[1]
+    seconds_per_scene = sys.argv[1]
 
 if len(sys.argv) >= 3:
     scenes_url = sys.argv[2]
@@ -21,6 +21,6 @@ if scenes_dict["status"] == "success":
         while True:
             for scene in scenes_dict["scenes"]:
                 req = requests.put(scenes_url, json={'id': str(scene), 'action': 'activate'})
-                time.sleep(int(time_per_scene))
+                time.sleep(seconds_per_scene)
     except KeyboardInterrupt:
         exit
